1e17d3ce913aeae1268258e6f964aa608cdf955e
EmilioPadilla <padillam_@hotmail.com>
2022-10-13 17:57:49 -0500

    setup again

--- .next/static/AbQ07Tv4ca6s-bgL2_4qj/_buildManifest.js
+++ .next/static/AbQ07Tv4ca6s-bgL2_4qj/_buildManifest.js
@@ -1,1 +1,7 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/ae0e3e027412e072.css","static/chunks/pages/index-44834fa983cbfac3.js"],"/_error":["static/chunks/pages/_error-19e73c3be68140bc.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
+(self.__BUILD_MANIFEST = {
+	__rewrites: {beforeFiles: [], afterFiles: [], fallback: []},
+	'/': ['static/css/ae0e3e027412e072.css', 'static/chunks/pages/index-44834fa983cbfac3.js'],
+	'/_error': ['static/chunks/pages/_error-19e73c3be68140bc.js'],
+	sortedPages: ['/', '/_app', '/_error']
+}),
+	self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB();
