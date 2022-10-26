206debb938587367e450bb31d74209d3ceafbafd
maneroto <maneroto@hotmail.com>
2022-10-26 12:18:45 -0500

    Add ThemeProvider

--- .next/server/pages/_app.js
+++ .next/server/pages/_app.js
@@ -5,7 +5,7 @@ exports.id = 888;
 exports.ids = [888];
 exports.modules = {
 
-/***/ 523:
+/***/ 445:
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 // ESM COMPAT FLAG
@@ -19,35 +19,68 @@ __webpack_require__.d(__webpack_exports__, {
 // EXTERNAL MODULE: external "next/head"
 var head_ = __webpack_require__(968);
 var head_default = /*#__PURE__*/__webpack_require__.n(head_);
-// EXTERNAL MODULE: external "styled-jsx/style"
-var style_ = __webpack_require__(816);
-var style_default = /*#__PURE__*/__webpack_require__.n(style_);
+;// CONCATENATED MODULE: external "@mui/material/styles/ThemeProvider"
+const ThemeProvider_namespaceObject = require("@mui/material/styles/ThemeProvider");
+var ThemeProvider_default = /*#__PURE__*/__webpack_require__.n(ThemeProvider_namespaceObject);
+;// CONCATENATED MODULE: external "@mui/material/CssBaseline"
+const CssBaseline_namespaceObject = require("@mui/material/CssBaseline");
+var CssBaseline_default = /*#__PURE__*/__webpack_require__.n(CssBaseline_namespaceObject);
+;// CONCATENATED MODULE: external "@mui/material/styles/createTheme"
+const createTheme_namespaceObject = require("@mui/material/styles/createTheme");
+var createTheme_default = /*#__PURE__*/__webpack_require__.n(createTheme_namespaceObject);
+;// CONCATENATED MODULE: ./src/themes/DefaultTheme.ts
+
+const DefaultTheme = createTheme_default()({
+  palette: {
+    primary: {
+      main: '#404041'
+    },
+    secondary: {
+      main: '#D3D3D3'
+    },
+    background: {
+      default: '#F3F3F3'
+    },
+    divider: '#D3D3D3',
+    text: {
+      primary: '#404041'
+    }
+  },
+  shape: {
+    borderRadius: 5
+  },
+  typography: {
+    h1: {
+      fontSize: '3.25rem'
+    },
+    h2: {
+      fontSize: '2.75rem'
+    },
+    h3: {
+      fontSize: '2.375rem'
+    },
+    subtitle1: {
+      fontSize: '1.75rem'
+    },
+    button: {
+      fontSize: '1.25rem'
+    },
+    fontSize: 16
+  },
+  components: {
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: 'capitalize',
+          padding: '0.75rem 2rem'
+        }
+      }
+    }
+  }
+});
+/* harmony default export */ const themes_DefaultTheme = (DefaultTheme);
 // EXTERNAL MODULE: external "react/jsx-runtime"
 var jsx_runtime_ = __webpack_require__(997);
-;// CONCATENATED MODULE: ./src/components/GlobalStyles/index.tsx
-
-
-
-
-const styles = [":root{--aqua_darkest:#004d77;--aqua_dark:#00698d;--aqua:#0078aa;--aqua_light:#009fcb;--aqua_lighter:#00cbed;--aqua_lightest:#5ee8ff;--white:#ffffff;--gray15:#f3f3f3;--gray14:#58575d;--gray13:#f8f8f8;--gray12:#f4f4f4;--gray11:#e7e7e7;--gray10:#dddddd;--gray9:#cccccc;--gray8:#a2aaad;--gray7:#999999;--gray6:#888888;--gray5:#707070;--gray4:#444444;--gray3:#333333;--gray2:#222222;--iris_blue:#00bbb4;--teal-blue:#00788c;--teal:#008480;--teal2:#008580;--persian_green:#00a39e;--plascon_sea:#005c58;--tenne:#cc4b00;--blue_fb:#295ca9;}", "*,*:before,*:after{box-sizing:border-box;}", "html{-webkit-scroll-behavior:smooth;-moz-scroll-behavior:smooth;-ms-scroll-behavior:smooth;scroll-behavior:smooth;font-size:62.5%;}", "body{background-color:var(--gray12);font-family:'SF Pro Display';font-size:1.6rem;line-height:2.5rem;margin:0;}", "html,body{box-sizing:border-box;height:100%;}", "body{display:block;}", "main{overflow-x:hidden;}", "section{grid-column:1 / -1;}", ".section-padding{padding:1.6rem;}", ".highlight-section-padding{padding:0 1.6rem 1.2rem 1.6rem;}", ".white-background{background-color:var(--white);}", ".gray-background{background-color:var(--gray12);}", ".persian_green-backgound{background-color:var(--persian_green);}", "hr{border:0;height:1px;border-top:1px solid var(--gray10);margin:0 1.6rem;}", ".bold{font-weight:bold;}", ".teal-txt{color:var(--teal);}", ".back-button{background-color:var(--gray9);border-radius:50%;height:30px;margin-left:16px;margin-top:70px;position:absolute;top:0;width:30px;}", ".header-container{padding-top:100px;text-align:center;}", ".header-title{text-transform:uppercase;font-weight:800;font-size:30px;}", ".header-content{font-size:16px;line-height:24px;padding:24px 16px;text-align:center;}", ".header-content-text{font-size:16px;line-height:19px;text-align:center;padding-bottom:32px;}", "h1,h2,h3,h4,h5,h6,p{margin:0;}", "a{color:inherit;cursor:pointer;-webkit-text-decoration:inherit;text-decoration:inherit;}", "h1{font-size:3.2rem;line-height:4.8rem;}", "h2{font-size:1.8rem;line-height:3.2rem;}", ".slider-container{overflow-y:scroll !important;}", ".swiper{margin-left:auto;margin-right:auto;position:relative;overflow:hidden;list-style:none;padding:0;z-index:0;}"];
-styles.__hash = "2063782172";
-function getFonts(assetsUrl = '/') {
-  const _defaultExport = [`@font-face{font-family:'SF Pro Display';src:url('${assetsUrl}assets/webfonts/SFProDisplay-BoldItalic.woff2') format('woff2'), url('${assetsUrl}assets/webfonts/SFProDisplay-BoldItalic.woff') format('woff');font-weight:bold;font-style:italic;font-display:swap;}`, `@font-face{font-family:'SF Pro Display';src:url('${assetsUrl}assets/webfonts/SFProDisplay-BlackItalic.woff2') format('woff2'), url('${assetsUrl}assets/webfonts/SFProDisplay-BlackItalic.woff') format('woff');font-weight:900;font-style:italic;font-display:swap;}`, `@font-face{font-family:'SF Pro Display';src:url('${assetsUrl}assets/webfonts/SFProDisplay-Heavy.woff2') format('woff2'), url('${assetsUrl}assets/webfonts/SFProDisplay-Heavy.woff') format('woff');font-weight:900;font-style:normal;font-display:swap;}`, `@font-face{font-family:'SF Pro Display';src:url('${assetsUrl}assets/webfonts/SFProDisplay-Bold.woff2') format('woff2'), url('${assetsUrl}assets/webfonts/SFProDisplay-Bold.woff') format('woff');font-weight:bold;font-style:normal;font-display:swap;}`, `@font-face{font-family:'SF Pro Display';src:url('${assetsUrl}assets/webfonts/SFProDisplay-Black.woff2') format('woff2'), url('${assetsUrl}assets/webfonts/SFProDisplay-Black.woff') format('woff');font-weight:900;font-style:normal;font-display:swap;}`, `@font-face{font-family:'SF Pro Display';src:url('${assetsUrl}assets/webfonts/SFProDisplay-Medium.woff2') format('woff2'), url('${assetsUrl}assets/webfonts/SFProDisplay-Medium.woff') format('woff');font-weight:500;font-style:normal;font-display:swap;}`, `@font-face{font-family:'SF Pro Display';src:url('${assetsUrl}assets/webfonts/SFProDisplay-LightItalic.woff2') format('woff2'), url('${assetsUrl}assets/webfonts/SFProDisplay-LightItalic.woff') format('woff');font-weight:300;font-style:italic;font-display:swap;}`, `@font-face{font-family:'SF Pro Display';src:url('${assetsUrl}assets/webfonts/SFProDisplay-Light.woff2') format('woff2'), url('${assetsUrl}assets/webfonts/SFProDisplay-Light.woff') format('woff');font-weight:300;font-style:normal;font-display:swap;}`, `@font-face{font-family:'SF Pro Display';src:url('${assetsUrl}assets/webfonts/SFProDisplay-HeavyItalic.woff2') format('woff2'), url('${assetsUrl}assets/webfonts/SFProDisplay-HeavyItalic.woff') format('woff');font-weight:900;font-style:italic;font-display:swap;}`, `@font-face{font-family:'SF Pro Display';src:url('${assetsUrl}assets/webfonts/SFProDisplay-Regular.woff2') format('woff2'), url('${assetsUrl}assets/webfonts/SFProDisplay-Regular.woff') format('woff');font-weight:normal;font-style:normal;font-display:swap;}`, `@font-face{font-family:'SF Pro Display';src:url('${assetsUrl}assets/webfonts/SFProDisplay-RegularItalic.woff2') format('woff2'), url('${assetsUrl}assets/webfonts/SFProDisplay-RegularItalic.woff') format('woff');font-weight:normal;font-style:italic;font-display:swap;}`, `@font-face{font-family:'SF Pro Display';src:url('${assetsUrl}assets/webfonts/SFProDisplay-Semibold.woff2') format('woff2'), url('${assetsUrl}assets/webfonts/SFProDisplay-Semibold.woff') format('woff');font-weight:600;font-style:normal;font-display:swap;}`, `@font-face{font-family:'SF Pro Display';src:url('${assetsUrl}assets/webfonts/SFProDisplay-SemiboldItalic.woff2') format('woff2'), url('${assetsUrl}assets/webfonts/SFProDisplay-SemiboldItalic.woff') format('woff');font-weight:600;font-style:italic;font-display:swap;}`, `@font-face{font-family:'SF Pro Display';src:url('${assetsUrl}assets/webfonts/SFProDisplay-MediumItalic.woff2') format('woff2'), url('${assetsUrl}assets/webfonts/SFProDisplay-MediumItalic.woff') format('woff');font-weight:500;font-style:italic;font-display:swap;}`, `@font-face{font-family:'SF Pro Display';src:url('${assetsUrl}assets/webfonts/SFProDisplay-ThinItalic.woff2') format('woff2'), url('${assetsUrl}assets/webfonts/SFProDisplay-ThinItalic.woff') format('woff');font-weight:100;font-style:italic;font-display:swap;}`, `@font-face{font-family:'SF Pro Display';src:url('${assetsUrl}assets/webfonts/SFProDisplay-Ultralight.woff2') format('woff2'), url('${assetsUrl}assets/webfonts/SFProDisplay-Ultralight.woff') format('woff');font-weight:100;font-style:normal;font-display:swap;}`, `@font-face{font-family:'SF Pro Display';src:url('${assetsUrl}assets/webfonts/SFProDisplay-UltralightItalic.woff2') format('woff2'), url('${assetsUrl}assets/webfonts/SFProDisplay-UltralightItalic.woff') format('woff');font-weight:100;font-style:italic;font-display:swap;}`, `@font-face{font-family:'SF Pro Display';src:url('${assetsUrl}assets/webfonts/SFProDisplay-Thin.woff2') format('woff2'), url('${assetsUrl}assets/webfonts/SFProDisplay-Thin.woff') format('woff');font-weight:100;font-style:normal;font-display:swap;}`];
-  _defaultExport.__hash = "1588412995";
-  return _defaultExport;
-}
-const GlobalStyles = () => {
-  const fonts = getFonts();
-  return /*#__PURE__*/(0,jsx_runtime_.jsxs)(jsx_runtime_.Fragment, {
-    children: [/*#__PURE__*/jsx_runtime_.jsx((style_default()), {
-      id: styles.__hash,
-      children: styles
-    }), /*#__PURE__*/jsx_runtime_.jsx((style_default()), {
-      id: fonts.__hash,
-      children: fonts
-    })]
-  });
-};
 ;// CONCATENATED MODULE: ./pages/_app.tsx
 function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); enumerableOnly && (symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; })), keys.push.apply(keys, symbols); } return keys; }
 
@@ -58,6 +91,8 @@ function _defineProperty(obj, key, value) { if (key in obj) { Object.definePrope
 /**
  * Main React parent component
  */
+
+
 
 
 
@@ -88,7 +123,12 @@ function App({
       }), /*#__PURE__*/jsx_runtime_.jsx("title", {
         children: "Zapata Connect"
       })]
-    }), "s", /*#__PURE__*/jsx_runtime_.jsx(GlobalStyles, {}), /*#__PURE__*/jsx_runtime_.jsx(Component, _objectSpread({}, pageProps))]
+    }), /*#__PURE__*/(0,jsx_runtime_.jsxs)((ThemeProvider_default()), {
+      theme: themes_DefaultTheme,
+      children: [/*#__PURE__*/jsx_runtime_.jsx((CssBaseline_default()), {
+        enableColorScheme: true
+      }), /*#__PURE__*/jsx_runtime_.jsx(Component, _objectSpread({}, pageProps))]
+    })]
   });
 }
 
@@ -108,13 +148,6 @@ module.exports = require("next/head");
 
 module.exports = require("react/jsx-runtime");
 
-/***/ }),
-
-/***/ 816:
-/***/ ((module) => {
-
-module.exports = require("styled-jsx/style");
-
 /***/ })
 
 };
@@ -124,7 +157,7 @@ module.exports = require("styled-jsx/style");
 var __webpack_require__ = require("../webpack-runtime.js");
 __webpack_require__.C(exports);
 var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-var __webpack_exports__ = (__webpack_exec__(523));
+var __webpack_exports__ = (__webpack_exec__(445));
 module.exports = __webpack_exports__;
 
 })();
